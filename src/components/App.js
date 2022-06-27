334ef6a6de3c908376c5f8b567a2a2db6b0cc778
LouisBassagal <louis.bassagal@epitech.eu>
2022-06-27 15:33:33 +0200

    [ADD] Adding several background according to the weather

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,34 @@ import React, { useState } from "react";
 import GetWeatherInformations from "./getWeatherInformations";
 import DisplayInformations from "./displayInformation";
 import CloudBG from "../assets/clouds.mp4"
+import RainBG from "../assets/rain.mp4"
+import SunnyBG from "../assets/sunny.mp4"
 import "../styles/App.css"
 
 function App() {
   const [data, setData] = useState({});
   return (
   <div className="weather-container">
+    { data.weather && data.weather[0].main === 'Clouds' ?
     <video className="weather-background" autoPlay loop muted>
       <source src={ CloudBG } type="video/mp4"/>
       Your browser doesn't support video tag ':(' !
-    </video>
+    </video> : null
+    }
+    { data.weather && data.weather[0].main === 'Rain' ? 
+    <video className="weather-background" autoPlay loop muted>
+    <source src={ RainBG } type="video/mp4"/>
+    Your browser doesn't support video tag !
+  </video>
+  : null
+  }
+  { data.weather && data.weather[0].main === 'Clear' ? 
+    <video className="weather-background" autoPlay loop muted>
+    <source src={ SunnyBG } type="video/mp4"/>
+    Your browser doesn't support video tag !
+  </video>
+  : null
+  }
     <GetWeatherInformations data={data} setData={setData} />
     <DisplayInformations data={data} />
   </div>
